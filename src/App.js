77bf442ae0bbf333a7304ea0e32f554da007af64
suzannah.thompson <suzannahthompson@hotmail.co.uk>
2024-01-12 15:34:02 +0000

    Working on shopping cart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./Components/Layout/Header";
 import {Fragment} from "react";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+        <main>
+            <Meals/>
+        </main>
     </Fragment>
   );
 }
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,12 +1,13 @@
 import meaalsImage from '../../assets/meals.jpg';
 import classes from "./Header.module.css";
 import {Fragment} from "react";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={meaalsImage} alt="A table full of delicious food!"/>
